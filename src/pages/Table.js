d944071d69bb96a2fa6965c736f897876d8aebb9
Ritik Sharma <rsharma@innow8apps.com>
2023-12-15 17:09:38 +0530

    fix: added no data found error in table

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Loader from "./Loader";
 
-const Table = ({ list, tableHeadings, detailHandler }) => {
+const Table = ({ list, tableHeadings, detailHandler, loading }) => {
+  console.log(tableHeadings, "tableHeadings??><KKJ");
+  console.log("list", list);
   return (
     <>
+      {loading && <Loader />}
       <div className="table-responsive">
         <table className="table table-striped">
           <thead>
@@ -15,7 +19,7 @@ const Table = ({ list, tableHeadings, detailHandler }) => {
             </tr>
           </thead>
           <tbody>
-            {list
+            {/* { list
               ?.sort((a, b) => {
                 return a.id - b.id;
               })
@@ -36,11 +40,36 @@ const Table = ({ list, tableHeadings, detailHandler }) => {
                     </td>
                   </tr>
                 );
-              })}
+              })} */}
+
+            {list && list.length > 0
+              ? list
+                  .sort((a, b) => a.id - b.id)
+                  .map((item, key) => (
+                    <tr key={key}>
+                      <td>{item.id}</td>
+                      <td>{item.name}</td>
+                      <td>{item.status ? item.status : "N/A"}</td>
+                      <td>{item.description}</td>
+                      <td className="details-td">
+                        <button
+                          className="btn btn-info detail-btn"
+                          onClick={() => detailHandler(item.id)}
+                        >
+                          Details
+                        </button>
+                      </td>
+                    </tr>
+                  ))
+              : !loading && (
+                  <tr>
+                    <td colSpan="5">No data found</td>
+                  </tr>
+                )}
           </tbody>
         </table>
       </div>
-      {!list && <div className="no-data"></div>}
+      {}
     </>
   );
 };
