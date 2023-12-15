ebf2ba248ecbf5ab7addd3cc9ecb27d511c31684
Ritik Sharma <rsharma@innow8apps.com>
2023-12-15 20:21:36 +0530

    minor bug fixes : TimeSheetTable

--- src/components/TimeSheetTable.js
+++ src/components/TimeSheetTable.js
@@ -2,16 +2,14 @@ import React from "react";
 import Loader from "../pages/Loader";
 
 const TimeSheetTable = (props) => {
-  const { timeSheets, isLoading, employeeOptions } = props;
-  const tableHeadings = ["Punch In", "Punch Out", "Crew Id", "User Name"];
+  const { timeSheets, isLoading } = props;
+  const tableHeadings = ["Punch In", "Punch Out", "Crew Id", "Crew User Id"];
   //   const getDate = (date) => {
   //     return new Date(date).getDate();
   //   };
   return (
     <div className="table-responsive crew-table">
-      {isLoading && (
-        <Loader /> // Render your loader component here
-      )}{" "}
+      {isLoading && <Loader />}{" "}
       <table className="table table-striped">
         <thead>
           {tableHeadings.map((item, key) => (
@@ -21,27 +19,21 @@ const TimeSheetTable = (props) => {
           ))}
         </thead>
         <tbody>
-          {timeSheets?.length ? (
-            timeSheets?.map((item, key) => {
-              const user = employeeOptions.find(
-                (user) => user.id === item.crew_user_id
-              );
-              
-              //   const punchIn = getDate(item.time_entry_in);
-              //   const punchOut = getDate(item.time_entry_out);
+          {timeSheets?.length
+            ? timeSheets?.map((item, key) => {
+                //   const punchIn = getDate(item.time_entry_in);
+                //   const punchOut = getDate(item.time_entry_out);
 
-              return (
-                <tr key={key}>
-                  <td>{item?.time_entry_in}</td>
-                  <td>{item?.time_entry_out}</td>
-                  <td>{item?.crew_id}</td>
-                  <td>{user?.name}</td>
-                </tr>
-              );
-            })
-          ) : (
-            <div>No data found</div>
-          )}
+                return (
+                  <tr key={key}>
+                    <td>{item?.time_entry_in}</td>
+                    <td>{item.time_entry_out}</td>
+                    <td>{item.crew_id}</td>
+                    <td>{item.crew_user_id}</td>
+                  </tr>
+                );
+              })
+            : !isLoading && <div>No data found</div>}
         </tbody>
       </table>
     </div>
