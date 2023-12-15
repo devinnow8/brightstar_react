d571a4bfb6e4f2b58578e33388ae50e79638d426
devinnow8 <devinnow8@gmail.com>
2023-12-15 18:05:29 +0530

    Update TimeSheetTable.js

--- src/components/TimeSheetTable.js
+++ src/components/TimeSheetTable.js
@@ -4,9 +4,9 @@ import AddCrewTimeDateModal from "./AddCrewTimeDateModal";
 const TimeSheetTable = (props) => {
   const { timeSheets } = props;
   const tableHeadings = ["Punch In", "Punch Out", "Crew Id", "Crew User Id"];
-  const getDate = (date) => {
-    return new Date(date).toUTCString();
-  };
+  //   const getDate = (date) => {
+  //     return new Date(date).getDate();
+  //   };
   return (
     <div className="table-responsive crew-table">
       <table className="table table-striped">
@@ -20,13 +20,13 @@ const TimeSheetTable = (props) => {
         <tbody>
           {timeSheets?.length ? (
             timeSheets?.map((item, key) => {
-              const punchIn = getDate(item.punch_in_date_time);
-              const punchOut = getDate(item.punch_out_date_time);
+              //   const punchIn = getDate(item.time_entry_in);
+              //   const punchOut = getDate(item.time_entry_out);
 
               return (
                 <tr key={key}>
-                  <td>{punchIn}</td>
-                  <td>{punchOut}</td>
+                  <td>{item?.time_entry_in}</td>
+                  <td>{item.time_entry_out}</td>
                   <td>{item.crew_id}</td>
                   <td>{item.crew_user_id}</td>
                 </tr>
